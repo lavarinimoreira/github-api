7fba2ffc12daed113bee1d4cf936067e4a7af314
Gabriel Lavarini <gabriel@lavarinimoreira.me>
2023-03-29 23:37:57 -0300

    updating app

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,21 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdSearch } from "react-icons/md";
 
 import { Container, Logo, Title, Form, Input, Button } from "./styles";
 
 import githubLogo from "../../assets/images/github-logo.svg";
 
-export default function Main() {
+const Main = () => {
+  const [login, setLogin] = useState("");
+
   return (
     <Container>
       <Logo src={githubLogo} alt="Github Logo" />
       <Title>Github API</Title>
       <Form>
-        <Input placeholder="user" />
+        <Input
+          placeholder="user"
+          value={login}
+          onChange={(e) => setLogin(e.target.value)}
+        />
         <Button>
           <MdSearch size={42} />
         </Button>
       </Form>
     </Container>
   );
-}
+};
+
+export default Main;
