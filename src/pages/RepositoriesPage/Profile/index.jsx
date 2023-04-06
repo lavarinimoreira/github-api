63e7d4f2325af386567b92537eba1a531b36ba7d
Gabriel Lavarini <gabriel@lavarinimoreira.me>
2023-04-06 17:31:14 -0300

    updating Profile

--- src/pages/RepositoriesPage/Profile/index.jsx
+++ src/pages/RepositoriesPage/Profile/index.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React from 'react';
 
-import { Container, Header, Avatar, Login, Name } from './styles';
+import { MdGroup, MdLocationCity, MdWork, MdLink } from 'react-icons/md';
+
+import { Container, Header, Avatar, Login, Name, Inner, Data } from './styles';
 
 const Profile = function() {
   return (
@@ -10,6 +12,23 @@ const Profile = function() {
         <Login>lavarinimorira</Login>
         <Name>Gabriel Lavarini</Name>
       </Header>
+      <Inner>
+        <Data>
+          <MdGroup size={20} />
+          30 <span>followers</span> 10 <span>following</span>
+        </Data>
+        <Data>
+          <MdWork size={20}/>
+          Software eng
+        </Data>
+        <Data>
+          <MdLocationCity size={20}/>Brazil
+        </Data>
+        <Data>
+          <MdLink size={20} />
+          <a href='https://lavarinimoreira.me'>site</a>
+        </Data>
+      </Inner>
     </Container>
   )
 }
--- src/pages/RepositoriesPage/Profile/styles.js
+++ src/pages/RepositoriesPage/Profile/styles.js
@@ -1,43 +1,73 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.div`
-    padding: 1rem;
+  padding: 1rem;
 `;
 
 export const Header = styled.div`
-    display: flex;
-    flex-direction: column;
-    padding: 1rem 0;
+  display: flex;
+  flex-direction: column;
+  padding: 1rem 0;
 
-    @media screen and (max-width: ${(props) => props.theme.breakpoints.md}){
-        flex-direction: row;
-        align-items: center;
-    }
+  @media screen and (max-width: ${(props) => props.theme.breakpoints.md}) {
+    flex-direction: row;
+    align-items: center;
+  }
 `;
 
 export const Avatar = styled.img`
-    align-self: center;
-    border-radius: 50%;
-    width: 70%;
-    margin-bottom: 1rem;
+  align-self: center;
+  border-radius: 50%;
+  width: 70%;
+  margin-bottom: 1rem;
 
-    @media screen and (max-width: ${(props) => props.theme.breakpoints.md}){
-        width: 70px;
-        height: 70px;
-        margin-right: 1rem;
-        margin-bottom: 0;
-    }
+  @media screen and (max-width: ${(props) => props.theme.breakpoints.md}) {
+    width: 70px;
+    height: 70px;
+    margin-right: 1rem;
+    margin-bottom: 0;
+  }
 `;
 
 export const Login = styled.h1`
-    font-size: ${(props) => props.theme.fontSizes.title};
+  font-size: ${(props) => props.theme.fontSizes.title};
+  margin: 0;
 `;
 
 export const Name = styled.h2`
-    font-size: ${(props) => props.theme.fontSizes.subtitle};
-    font-weight: normal;
+  font-size: ${(props) => props.theme.fontSizes.subtitle};
+  font-weight: normal;
 
-    @media screen and (max-width: ${(props) => props.theme.breakpoints.md}){
-        display: none;
-    }
+  @media screen and (max-width: ${(props) => props.theme.breakpoints.md}) {
+    display: none;
+  }
+`;
+
+export const Inner = styled.div`
+  padding: 1rem 0;
+  a {
+    text-decoration: none;
+    color: ${(props) => props.theme.colors.text};
+  }
+
+  span {
+    color: ${(props) => props.theme.colors.gray600};
+    margin: 0 4px 0 4px;
+  }
+  
+  @media screen and (max-width: ${(props) => props.theme.breakpoints.md}) {
+    display: none;
+  }
+`;
+
+export const Data = styled.p`
+  display: flex;
+  align-items: center;
+  margin: 0 0 6px 0;
+  font-size: ${(props) => props.theme.fontSizes.small};
+  
+  svg {
+    margin-right: 10px;
+    color: ${(props) => props.theme.colors.gray600};
+  }
 `;
