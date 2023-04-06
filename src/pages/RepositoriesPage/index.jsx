93841cc04a2d6ff489eadac8dd838df10f3a6e14
Gabriel Lavarini <gabriel@lavarinimoreira.me>
2023-04-06 15:22:25 -0300

    adding repositories page

---
+++ src/pages/RepositoriesPage/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container, Sidebar, Main } from './styles';
+
+function RepositoriesPage() {
+  return (
+    <Container>
+      <Sidebar>Sidebar</Sidebar>
+      <Main>Main</Main>
+    </Container>
+  )
+};
+
+export default RepositoriesPage;
